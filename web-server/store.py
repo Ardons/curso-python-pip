25b5014f9a3654e7a7efb4724105e15c980f8d16
Ardons <alca.amb.96@gmail.com>
2025-02-20 20:42:42 -0500

    cambiose en el proyecto de web-server

--- web-server/store.py
+++ web-server/store.py
@@ -2,9 +2,10 @@ import requests
 
 def get_categories():
     r = requests.get('https://api.escuelajs.co/api/v1/categories')
-    print(r.status_code)
-    print(r.text)
-    print(type(r.text))
     categories = r.json()
+    print(categories)
+    print(type(categories))
+    
     for category in categories:
-        print(category['name'])
+        title = category['name']
+        print(title)
